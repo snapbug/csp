8626d77fa425062af9f1a79b5d1fb5d6110ee41d
Matt Crane <mcrane@cs.otago.ac.nz>
2010-07-05 17:01:45 +1200

    Output slight change

--- re_run_samples.py
+++ re_run_samples.py
@@ -8,4 +8,5 @@ for s in range(nsamples):
 	us = file('output/sample.%d.gp.pk.mae.txt' % s, 'r').next().strip().split()
 	p = subprocess.Popen(['./bin/csp', '-e', '-pk', '-gg', '-seE'] + us, stdout=subprocess.PIPE).communicate()
 	print >> file('output/sample.%d.gg(exhaust).pk.mae.txt' % s, 'w'), p[0]
+	print
 	print '%s %d %s' % ('-'*15, s, '-'*15)
--- run_samples.py
+++ run_samples.py
@@ -12,4 +12,5 @@ for s in range(nsamples):
 	us = map(str, sorted(u[s * ssize:(s + 1) * ssize]))
 	p = subprocess.Popen(['./bin/csp', '-e', '-pk', '-gp', '-seE'] + us, stdout=subprocess.PIPE).communicate()
 	print >> file('output/sample.%d.gp.pk.mae.txt' % s, 'w'), p[0]
+	print
 	print '%s %d %s' % ('-'*15, s, '-'*15)
