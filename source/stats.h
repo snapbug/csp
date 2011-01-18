4385c3992a2472bbbb6b30341ea56225bcdc2701
Matt Crane <mcrane@cs.otago.ac.nz>
2011-01-19 11:28:52 +1300

    Added stats constants

--- source/stats.h
+++ source/stats.h
@@ -13,12 +13,15 @@ public:
 	virtual ~CSP_stats() {}
 	
 	enum {
-		NONE                = 0,
-		GENERATION_TIME     = 1,
-		PREDICTION_TIME     = 2,
-		ERROR_PRESENTED     = 4,
-		ERROR_RATED         = 8,
-		AUC                 = 16
+		NONE            = 0,
+		GENERATION_TIME = 1,
+		PREDICTION_TIME = 2,
+		ERROR_PRESENTED = 4,
+		ERROR_RATED     = 8,
+		AUC             = 16,
+		HIT_RATE        = 32,
+		FINISH          = 64,
+		TTEST           = 128
 	};
 
 public:
